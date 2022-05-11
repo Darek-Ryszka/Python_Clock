06633dfbb9d120f7fdf341216a8e4d0ace70d116
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-05-11 08:39:41 +0200

    Add day month year label

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from time import *
 
+
 def update():
     time_string = strftime("%H:%M:%S %p")
     time_label.config(text=time_string)
@@ -8,15 +9,24 @@ def update():
     day_string = strftime("%A")
     day_label.config(text=day_string)
 
-window = Tk()
+    date_string = strftime("%d %B %Y")
+    date_label.config(text=date_string)
 
-time_label = Label(window,font=("Arial",50),fg="#00FF00",bg="black")
+    window.after(1000, update)
+
+
+window = Tk(className='Python clock program')
+
+window.configure(bg='black')
+
+time_label = Label(window, font=("Arial", 50), fg="#00FF00", bg="black")
 time_label.pack()
 
-day_label = Label(window,font=("Arial",25))
-day_label.pack()
+date_label = Label(window, font=("Arial", 30), fg="#00FF00", bg="black")
+date_label.pack()
 
-window.after(1000,update)
+day_label = Label(window, font=("Arial", 25), fg="#00FF00", bg="black")
+day_label.pack()
 
 update()
 

ed58838592dff4c10a50ce3f84b9b52b870715b1
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-05-11 08:27:56 +0200

    Add day information

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ def update():
     time_string = strftime("%H:%M:%S %p")
     time_label.config(text=time_string)
 
+    day_string = strftime("%A")
+    day_label.config(text=day_string)
+
 window = Tk()
 
 time_label = Label(window,font=("Arial",50),fg="#00FF00",bg="black")
 time_label.pack()
+
+day_label = Label(window,font=("Arial",25))
+day_label.pack()
 
 window.after(1000,update)
 

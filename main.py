5df3cc890aa8504cd9c3865b6e4bc2d9436e81ec
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-05-11 08:21:55 +0200

    Add function update

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from tkinter import *
 from time import *
 
+def update():
+    pass
+
 window = Tk()
 
 time_label = Label(window,font=("Arial",50),fg="#00FF00",bg="black")
 time_label.pack()
+
+update()
 
 window.mainloop()
